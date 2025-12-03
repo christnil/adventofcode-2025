ed8db29d5f67b25cc36a40f661fdabc7bb202cf4
Christoffer Nilsson <christoffer.nilsson@svenskaspel.se>
2025-12-03 08:35:35 +0100

    solve day3 part1

--- lib/aoc/days/day_03.ex
+++ lib/aoc/days/day_03.ex
@@ -8,17 +8,41 @@ defmodule Aoc.Days.Day03 do
   @day 3
 
   @doc """
+  split string into list of numbers
+  """
+  def tokenize_line(line) do
+    line
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @doc """
+  Find max
+  """
+  def find_max(list, max1 \\ 0, max2 \\ 0) do
+    case list do
+      [] -> (max1 * 10) + max2
+      [h | t] when max2 > max1 -> find_max(t, max2, h)
+      [h | t] when h > max2 -> find_max(t, max1, max(max2, h))
+      [_ | t] -> find_max(t, max1, max2)
+    end
+  end
+
+  @doc """
   Placeholder Part 1: sum all numbers.
   """
   def part1(data) do
-    Enum.sum(data)
+    data
+    |> Enum.map(&tokenize_line/1)
+    |> Enum.map(&find_max/1)
+    |> Enum.sum()
   end
 
   @doc """
   Placeholder Part 2: currently same as Part 1.
   """
-  def part2(data) do
-    Enum.sum(data)
+  def part2(_data) do
+    0
   end
 
   @doc """
@@ -27,7 +51,7 @@ defmodule Aoc.Days.Day03 do
   Returns {part1_result, part2_result}.
   """
   def run(type \\ :input) do
-    data = Input.integers(@day, type)
+    data = Input.lines(@day, type)
     {part1(data), part2(data)}
   end
 end
--- test/aoc/day_03_test.exs
+++ test/aoc/day_03_test.exs
@@ -6,23 +6,39 @@ defmodule Aoc.Days.Day03Test do
 
   @day 3
 
+  test "tokenizes line into list of numbers" do
+    line = "12345"
+    assert Day03.tokenize_line(line) == [1, 2, 3, 4, 5]
+
+    line = "9081726354"
+    assert Day03.tokenize_line(line) == [9, 0, 8, 1, 7, 2, 6, 3, 5, 4]
+  end
+
+  test "finds max two numbers correctly" do
+    assert Day03.find_max([1, 2, 3, 4, 5]) == 45
+    assert Day03.find_max([5, 4, 3, 2, 1]) == 54
+    assert Day03.find_max([3, 1, 4, 1, 5, 9, 2, 6, 5]) == 96
+    assert Day03.find_max([9, 8, 7, 6, 5]) == 98
+    assert Day03.find_max([]) == 0
+  end
+
   test "part1 on test input sums numbers" do
-    data = Input.integers(@day, :test)
-    assert Day03.part1(data) == 9
+    data = Input.lines(@day, :test)
+    assert Day03.part1(data) == 357
   end
 
   test "part2 on test input sums numbers" do
-    data = Input.integers(@day, :test)
-    assert Day03.part2(data) == 9
+    data = Input.lines(@day, :test)
+    assert Day03.part2(data) == 0
   end
 
   test "part1 on real input returns an integer" do
-    data = Input.integers(@day, :input)
+    data = Input.lines(@day, :input)
     assert is_integer(Day03.part1(data))
   end
 
   test "part2 on real input returns an integer" do
-    data = Input.integers(@day, :input)
+    data = Input.lines(@day, :input)
     assert is_integer(Day03.part2(data))
   end
 end
