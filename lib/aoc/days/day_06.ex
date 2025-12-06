7a0b641bf179540d097c7a6e0cd1717af6c03742
Christoffer Nilsson <christoffer.nilsson@svenskaspel.se>
2025-12-06 08:39:40 +0100

    fix: day6 part1

--- lib/aoc/days/day_06.ex
+++ lib/aoc/days/day_06.ex
@@ -7,18 +7,40 @@ defmodule Aoc.Days.Day06 do
 
   @day 6
 
+  def get_data(type \\ :input) do
+    Input.lines(@day, type)
+    |> Enum.map(&String.split(&1, " ", trim: true))
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.map(fn [h | t] ->
+      {
+        h,
+        Enum.map(t, &String.to_integer/1)
+      }
+    end)
+  end
+
   @doc """
   Placeholder Part 1: sum all numbers.
   """
   def part1(data) do
-    Enum.sum(data)
+    data
+    |> Enum.map(fn {op, list} ->
+      if op == "+" do
+        Enum.sum(list)
+      else
+        Enum.product(list)
+      end
+    end)
+    |> Enum.sum()
   end
 
   @doc """
   Placeholder Part 2: currently same as Part 1.
   """
   def part2(data) do
-    Enum.sum(data)
+    0
   end
 
   @doc """
@@ -27,7 +49,7 @@ defmodule Aoc.Days.Day06 do
   Returns {part1_result, part2_result}.
   """
   def run(type \\ :input) do
-    data = Input.integers(@day, type)
+    data = get_data(type)
     {part1(data), part2(data)}
   end
 end
--- test/aoc/day_06_test.exs
+++ test/aoc/day_06_test.exs
@@ -2,27 +2,24 @@ defmodule Aoc.Days.Day06Test do
   use ExUnit.Case, async: true
 
   alias Aoc.Days.Day06
-  alias Aoc.Input
-
-  @day 6
 
   test "part1 on test input sums numbers" do
-    data = Input.integers(@day, :test)
-    assert Day06.part1(data) == 18
+    data = Day06.get_data(:test)
+    assert Day06.part1(data) == 4277556
   end
 
   test "part2 on test input sums numbers" do
-    data = Input.integers(@day, :test)
-    assert Day06.part2(data) == 18
+    data = Day06.get_data(:test)
+    assert is_integer(Day06.part2(data))
   end
 
   test "part1 on real input returns an integer" do
-    data = Input.integers(@day, :input)
-    assert is_integer(Day06.part1(data))
+    data = Day06.get_data(:input)
+    assert Day06.part1(data) == 8108520669952
   end
 
   test "part2 on real input returns an integer" do
-    data = Input.integers(@day, :input)
+    data = Day06.get_data(:input)
     assert is_integer(Day06.part2(data))
   end
 end
